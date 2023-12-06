b3da5f5e9937f362404bd84962f4b96bf743c130
CodeFox1419 <codefox1419@gmail.com>
2023-12-06 12:50:12 +0530

    Modern ui/ux Page Design for Pizza-App

---
+++ Final Project/data.js
@@ -0,0 +1,73 @@
+export const data = [
+    {
+        "id": 661066,
+        "type": "#Vegetarian",
+        "rating": 3.6,
+        "ingredients": [
+                        "<p>Basic Pizza Dough</p>",
+                        "<p>olive oil</p>",
+                        "<p>fat-free bottled pizza sauce</p>",
+                        "<p>shredded part-skim mozzarella cheese</p>",
+                        "<p>grated fresh Parmesan cheese</p>"
+                    ],
+        "title": "Cheese Pizza",
+        "image": "./img/cheesepizza.png",
+        "summary": "It should be no shocker that a classic is the statistical favorite. Cheese pizza is one of the most popular choices. It will always be a simple, unadorned masterpiece on its own.",
+        "cuisines": ["Italian"]
+    },
+    {
+        "id": 661068,
+        "type": "#Vegetarian",
+        "rating": 4.8,
+        "ingredients": [
+                        "<p>Pizza Dough</p>",
+                        "<p>Cornmeal</p>",
+                        "<p>Pizza sauce</p>",
+                        "<p>Asiago, pecorino, and mozzarella cheese</p>",
+                        "<p>Roasted red peppers</p>",
+                        "<p>Artichoke hearts</p>",
+                        "<p>Red onion</p>",
+                        "<p>Jalapeño pepper</p>",
+                        "<p>Extra-virgin olive oil</p>",
+                        "<p>fresh basil leaves</p>"
+                    ],
+        "title": "Veggie Pizza",
+        "image": "./img/veggiepizza.png",
+        "summary": "veggies are the perfect topping. And you’re only limited by your imagination. Everything from peppers and mushrooms, to eggplant and onions make for an exciting and tasty veggie pizza.",
+        "cuisines": ["Italian"]
+    },
+    {
+        "id": 661070,
+        "type": "#Vegetarian",
+        "rating": 5,
+        "ingredients": [
+                        "<p>Homemade pizza dough</p>",
+                        "<p>pizza sauce</p>",
+                        "<p>shredded mozzarella cheese</p>",
+                        "<p>diced deli ham</p>",
+                        "<p>fresh diced pineapple</p>",
+                        "<p>Dried oregano</p>",
+                        "<p>Melted garlic butter</p>"
+                    ],
+        "title": "Hawalian Pizza",
+        "image": "./img/hawalianpizza.png",
+        "summary": "HawaLian pizza is a pizza originating in Canada, and is traditionally topped with pineapple, tomato sauce, cheese, and either ham or bacon.",
+        "cuisines": ["Canadian"]
+    },
+    {
+        "id": 661072,
+        "type": "#Vegetarian",
+        "rating": 5,
+        "ingredients": [
+                        "<p>Pizza dough</p>",
+                        "<p>San Marzano Tomatoes</p>",
+                        "<p>Fresh Mozzarella Balls</p>",
+                        "<p>Fresh Basil</p>",
+                        "<p>Olive Oil & Salt</p>"
+                    ],
+        "title": "Margherita Pizza",
+        "image": "./img/margheritapizza.png",
+        "summary": "Deceptively simple, the Margherita pizza is made with basil, fresh mozzarella, and tomatoes. There’s a reason it’s an Italian staple and one of the most popular types of pizza in the country.",
+        "cuisines": ["Italian"]
+    },
+]
---
+++ Final Project/main.js
@@ -0,0 +1,211 @@
+import { data } from "./data.js";
+const cards = document.querySelectorAll('.card');
+const textBox = document.querySelectorAll('.img,.dess,.head,.cui,.list,.rat-num,.info-head,.info-para')
+const link = document.querySelectorAll('.link');
+const like = document.getElementById('like');
+const dislike = document.getElementById('dislike');
+
+// like & dislike functionality
+
+like.addEventListener('click',(e)=>{
+    if(dislike.classList.contains('fa-solid')){
+        dislike.classList.remove('fa-solid')
+    }
+    like.classList.toggle('fa-solid')
+})
+dislike.addEventListener('click',(e)=>{
+    if(like.classList.contains('fa-solid')){
+        like.classList.remove('fa-solid')
+    }
+    dislike.classList.toggle('fa-solid')
+})
+
+//  functionality to show clicked card data in display section
+
+cards.forEach((e)=>{
+    e.addEventListener('click',(e)=>{
+        const { id, parentNode } = e.target;
+            for (var i = 0; i < data.length; i++) {
+                if (data[i].id == id || data[i].id == parentNode.id) {
+                    cards[i].classList.add('sel');
+                    textBox[0].classList.add("img_a")
+                    setTimeout(animate,500,i)
+                    setTimeout(addData,500,i);
+                    textBox[0].addEventListener('animationend',()=>{
+                        textBox[0].classList.remove("img_a")
+                    })                
+                }
+                else{
+                    cards[i].classList.remove('sel')
+                }
+            }
+    })
+})
+
+// function to add data of clicked card to display section 
+
+const addData = (i)=>{
+    textBox[0].src = data[i].image;
+    textBox[1].textContent = data[i].type;
+    textBox[2].textContent = data[i].title;
+    textBox[3].textContent = data[i].cuisines[0];
+    textBox[4].innerHTML = data[i].ingredients.join('\n');
+    textBox[5].textContent = data[i].rating;
+    textBox[6].textContent = data[i].title;
+    textBox[7].textContent = data[i].summary;
+}
+
+//  function to add data to cards
+
+const addCardData = ()=>{
+    data.map((e,i)=>{
+        cards[i].attributes[1].textContent = e.id
+        cards[i].children[0].src = e.image;
+        cards[i].children[1].textContent = e.title
+    })
+    textBox[0].src = data[0].image;
+    textBox[1].textContent = data[0].type;
+    textBox[2].textContent = data[0].title;
+    textBox[3].textContent = data[0].cuisines[0];
+    textBox[4].innerHTML = data[0].ingredients.join('\n');
+    textBox[5].textContent = data[0].rating;
+    textBox[6].textContent = data[0].title;
+    textBox[7].textContent = data[0].summary;
+} 
+
+// Adding data on page load
+
+window.addEventListener('load',()=>{
+    addCardData();
+})
+
+// functionality to show or hide content
+
+link.forEach((e)=>{
+    e.addEventListener("click",(e)=>{
+        e.preventDefault()
+        if(e.target.id === "over"){
+            document.querySelector('.content').classList.remove('show')
+            document.querySelector('.i-info').classList.add('show')
+        }else if(e.target.id === 'ingr'){
+            document.querySelector('.i-info').classList.remove('show')
+            document.querySelector('.content').classList.add('show')
+        }
+    })
+})
+
+// Animation on Load
+
+window.addEventListener('load',()=>{
+    const tl = gsap.timeline();
+    tl.set('.nav',{
+        opacity:0,
+        y:-100,
+    }).to('.nav',{
+        y:0,
+        opacity:1,
+        delay:0.6,
+        ease: "power2.out"
+    })
+    gsap.from('.des',{
+        y:150,
+        opacity:0,
+        delay:1,
+        duration:2,
+        ease: "power2.out"
+    })
+    gsap.from('.dess',{
+        y:-150,
+        opacity:0,
+        delay:1,
+        duration:2,
+        ease: "power2.out"
+    })
+    gsap.from('.head',{
+        x:150,
+        opacity:0,
+        delay:1,
+        duration:2,
+        ease: "power2.out"
+    })
+    gsap.from('.con',{
+        y:150,
+        opacity:0,
+        delay:1,
+        duration:2,
+        ease: "power2.out"
+    })
+    gsap.from('.card-wrapper',{
+        y:150,
+        opacity:0,
+        delay:1,
+        duration:2,
+        ease: "power2.out"
+    })
+    gsap.from('.img',{
+        keyframes: {
+            "0%":   { opacity:0,y: "300%",x: "-500%"},
+            "75%":  { opacity:0,y: "50%",x: "-250%"},
+            "100%": { y: 0 ,x: 0,opacity:1},
+           },
+           ease: 'none',
+           duration:2,
+    })
+    gsap.from('.circle',{
+        keyframes: {
+            "0%":   { opacity:0,y: "300%",x: "-500%"},
+            "75%":  { opacity:0,y: "50%",x: "-250%"},
+            "100%": { y: 0 ,x: 0,opacity:1},
+           },
+           ease: 'none',
+           duration:2
+    })
+})
+
+//  Animation for elements when they update
+
+const animate = (i)=>{
+    const clArr = ["#ffa166","#fed766","#fec89a","#ff9b54","#F8DBB4"];
+    var clr = clArr[Math.floor(Math.random() * clArr.length)]
+    gsap.set('#path',{
+        fill: clr,
+        ease: "linear"
+    })
+    gsap.set('#rating',{
+        backgroundColor: clr,
+        ease: "linear"
+    })
+    if(i%2 == 0){
+        gsap.from('.com_e',{
+            y: 10,
+            opacity: 0,
+            stagger: 0.4
+        })
+        gsap.from('.com_i_e',{
+            x: 10,
+            opacity: 0,
+            stagger: 0.4
+        })
+        gsap.from('.rat-num',{
+            y: 30,
+            opacity: 0,
+            ease: 'linear'
+        })
+    }else{
+        gsap.from('.com_e',{
+            y: -10,
+            opacity: 0,
+            stagger: 0.4
+        })
+        gsap.from('.com_i_e',{
+            x: -10,
+            opacity: 0,
+            stagger: 0.4
+        })
+        gsap.from('.rat-num',{
+            y: -30,
+            opacity: 0,
+            ease: 'linear'
+        })
+    }
+}
